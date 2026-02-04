bffca8ae7d327351b0644089bfb7f8677d76264d
unknown <nami0879@naver.com>
2026-02-04 18:49:08 +0900

    Merge Intervals

---
+++ LeetCode/Medium/Merge_Intervals.cpp
@@ -0,0 +1,78 @@
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+// and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+
+// Example 2:
+
+// Input: intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+// Example 3:
+
+// Input: intervals = [[4,7],[1,4]]
+// Output: [[1,7]]
+// Explanation: Intervals [1,4] and [4,7] are considered overlapping.
+
+// Constraints:
+
+// 1 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 104
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+		sort(intervals.begin(), intervals.end(),
+			[](const vector<int>& a, const vector<int>& b) {
+				return a[0] < b[0];
+			});
+
+		vector<vector<int>> answer;
+		answer.push_back(intervals[0]);
+
+		for (int i = 1; i < intervals.size(); ++i) {
+			auto& last = answer.back();
+
+			if (intervals[i][0] > last[1]) {
+				answer.push_back(intervals[i]);
+			} else {
+				last[1] = max(last[1], intervals[i][1]);
+			}
+		}
+
+		return answer;
+	}
+};
+
+int main() {
+	// vector<vector<int>> intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+	vector<vector<int>> intervals = {{1, 4}, {4, 5}};
+
+	Solution s;
+	vector<vector<int>> answer = s.merge(intervals);
+
+	printf("answer: \n");
+	for (int i = 0; i < answer.size(); ++i) {
+		for (int j = 0; j < answer[i].size(); ++j) {
+			printf("%d ", answer[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+// Runtime: 3ms 89.23%
+// Memory: 23.64MB 93.69%
