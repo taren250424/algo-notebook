8a3e4d901540590a4d31689f6bf6412c9d350e57
unknown <nami0879@naver.com>
2026-02-24 20:41:41 +0900

    Scramble String

---
+++ LeetCode/Hard/Scramble_String.cpp
@@ -0,0 +1,94 @@
+// We can scramble a string s to get a string t using the following algorithm:
+
+// If the length of the string is 1, stop.
+// If the length of the string is > 1, do the following:
+// Split the string into two non-empty substrings at a random index, i.e., if the string is s, divide it to x and y where s = x + y.
+// Randomly decide to swap the two substrings or to keep them in the same order. i.e., after this step, s may become s = x + y or s = y + x.
+// Apply step 1 recursively on each of the two substrings x and y.
+// Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1, otherwise, return false.
+
+// Example 1:
+
+// Input: s1 = "great", s2 = "rgeat"
+// Output: true
+// Explanation: One possible scenario applied on s1 is:
+// "great" --> "gr/eat" // divide at random index.
+// "gr/eat" --> "gr/eat" // random decision is not to swap the two substrings and keep them in order.
+// "gr/eat" --> "g/r / e/at" // apply the same algorithm recursively on both substrings. divide at random index each of them.
+// "g/r / e/at" --> "r/g / e/at" // random decision was to swap the first substring and to keep the second substring in the same order.
+// "r/g / e/at" --> "r/g / e/ a/t" // again apply the algorithm recursively, divide "at" to "a/t".
+// "r/g / e/ a/t" --> "r/g / e/ a/t" // random decision is to keep both substrings in the same order.
+// The algorithm stops now, and the result string is "rgeat" which is s2.
+// As one possible scenario led s1 to be scrambled to s2, we return true.
+
+// Example 2:
+
+// Input: s1 = "abcde", s2 = "caebd"
+// Output: false
+
+// Example 3:
+
+// Input: s1 = "a", s2 = "a"
+// Output: true
+
+// Constraints:
+
+// s1.length == s2.length
+// 1 <= s1.length <= 30
+// s1 and s2 consist of lowercase English letters.
+
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	string S1;
+	string S2;
+	int memo[31][31][31];
+
+	bool dfs(int i1,int i2, int len) {
+		if (memo[i1][i2][len] != -1) return memo[i1][i2][len];
+		if ( S1.compare(i1, len, S2, i2, len) == 0 ) return memo[i1][i2][len] = 1;
+		
+		int count[26] = { 0 };
+		for (int i = 0; i < len; ++i) {
+			count[S1[i1 + i] - 'a']++;
+			count[S2[i2 + i] - 'a']--;
+		}
+		for (int c : count) if (c != 0) return memo[i1][i2][len] = 0;
+
+		for (int i = 1; i < len; ++i) {
+			bool f1 = dfs(i1, i2, i);
+			bool f2 = dfs(i1 + i, i2 + i, len - i);
+			if (f1 && f2) return memo[i1][i2][len] = 1;
+
+			bool r1 = dfs(i1, i2 + len - i, i);
+			bool r2 = dfs(i1 + i, i2, len - i);
+			if (r1 && r2) return memo[i1][i2][len] = 1;
+		}
+
+		return memo[i1][i2][len] = 0;
+	}
+
+	bool isScramble(string s1, string s2) {
+		S1 = s1, S2 = s2;
+		memset(memo, -1, sizeof(memo));
+		return dfs(0, 0, s1.length());
+	}
+};
+
+int main() {
+	string s1 = "great";
+	string s2 = "rgeat";
+
+	Solution s;
+	bool answer = s.isScramble(s1, s2);
+	printf("answer: %d\n", answer);
+
+	return 0;
+}
+
+// Runtime: 2ms 97.75%
+// Memory: 9.47MB 92.33%
